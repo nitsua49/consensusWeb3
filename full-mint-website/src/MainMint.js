5a2088ee7fc12c00790106c6d0b92b24091eeb23
nitsua49 <118407355+nitsua49@users.noreply.github.com>
2022-11-30 00:20:09 -0500

    Deployed website

--- full-mint-website/src/MainMint.js
+++ full-mint-website/src/MainMint.js
@@ -39,7 +39,7 @@ const MainMint = ({ accounts, setAccounts }) => {
     };
 
     return(
-        <div>
+        <div class="container mintBody">
             <h1>Consensus Digital Media</h1>
             <p>Be part of our exclusive Web 3 membership and get access to our exclusive stories and other membership benefits in the future. Mint your membership NFT today!</p>
             {isConnected ? (
