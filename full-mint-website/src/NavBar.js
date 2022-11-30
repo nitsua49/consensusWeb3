b50041cf3d6964a9123dc1ff7d40f0c431cb2024
nitsua49 <118407355+nitsua49@users.noreply.github.com>
2022-11-29 23:20:08 -0500

    Deployed website

--- full-mint-website/src/NavBar.js
+++ full-mint-website/src/NavBar.js
@@ -21,7 +21,7 @@ const NavBar = ({ accounts, setAccounts }) => {
 
             {/* Right Side - Sections */}
             <div class="topnav">
-                <a class="active" href="#home"><img src="Consensus_Logo_Final-WHITE.png.webp" class="logoImg" alt="CONSENSUS"/></a>
+                <a class="active" href="#home"><img src="assests/Consensus_Logo_Final-WHITE.png.webp" class="logoImg" alt="CONSENSUS"/></a>
                 <div class="topnav-right">
                 <a href="App.js">Membership</a>
                 <a href="#about">Team</a>
